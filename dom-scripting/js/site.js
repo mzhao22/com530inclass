9508f2e9d7f9945c6a8f9dc95266cc293098c8a6
Mia mia <aceline0326@gmail.com>
2017-11-07 20:40:07 -0600

    webpages, links, lint

--- dom-scripting/js/site.js
+++ dom-scripting/js/site.js
@@ -4,3 +4,19 @@ $('a[href^="http"]').on('click', function(event) {
   window.open($(this).attr('href'), "_blank");
   event.preventDefault();
 });
+
+$('abbr[title]').on('click', function(){
+  var expansion = $(this).attr('title');
+  var abbr = $(this).text()
+  $(this).text(expansion +" (" + abbr +")");
+  $(this).addClass('is-expanded')
+});
+
+$($('li')[2]).on('click', function(){
+  $(this).addClass('is-faded-out');
+});
+
+$('#doorbell')on('click', function(){
+  var doorbell = new Audio('media/doorbell.mp3')
+  doorbell.play();
+});
